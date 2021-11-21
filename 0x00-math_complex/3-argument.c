ffd9cb1128df497042cc73a87aaa6760b72b51f5
Luis Gonzales <luisgonzacc@gmail.com>
2021-11-21 05:14:46 +0000

    Task 3

---
+++ 0x00-math_complex/3-argument.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include <math.h>
+
+/**
+* argument - return the argument
+* @c: structure complex
+* Return: double a
+*/
+
+double argument(complex c)
+{
+double a;
+a = atan(c.im / c.re);
+return (a);
+}
