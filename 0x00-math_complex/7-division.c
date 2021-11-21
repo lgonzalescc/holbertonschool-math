8621212fdf40e260cf469858b74fc58c1db4657d
Luis Gonzales <luisgonzacc@gmail.com>
2021-11-21 06:12:37 +0000

    fixing task 7

--- 0x00-math_complex/7-division.c
+++ 0x00-math_complex/7-division.c
@@ -2,22 +2,20 @@
 #include <math.h>
 
 /**
-* division - return the division
-* @c1: complex
-* @c2: complex
-* @c3: complex pointer
-* Return: void
-*/
+ * division - perform the division operation to complex numbers
+ * @c1: first complex number
+ * @c2: second complex number
+ * @c3: quotient of complex numbers
+ * Return: void
+ */
 
 void division(complex c1, complex c2, complex *c3)
 {
-/* all at once */
-double a = c1.re;
-double b = c1.im;
+	double a = c1.re;
+	double b = c1.im;
+	double p = c2.re;
+	double q = c2.im;
 
-double c = c2.re;
-double d = c2.im;
-/* to structure */
-c3->re = ((a * c) + (b * d)) / (pow(c, 2) + pow(d, 2));
-c3->im = ((b * c) - (a * d)) / (pow(c, 2) + pow(d, 2));
+	c3->re = ((a * p) + (b * q)) / (pow(p, 2) + pow(q, 2));
+	c3->im = ((b * p) - (a * q)) / (pow(p, 2) + pow(q, 2));
 }
