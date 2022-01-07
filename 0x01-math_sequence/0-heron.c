4a93e4d884ced7f86badd8f59d980390ab1bd4b1
Luis <luisgonzacc@gmail.com>
2022-01-07 03:31:01 +0000

    fixing code

--- 0x01-math_sequence/0-heron.c
+++ 0x01-math_sequence/0-heron.c
@@ -1,75 +1,46 @@
-#include <math.h>
 #include "heron.h"
-
 /**
-* add_node - add node
-* @tpt: pointer
-* @v: double
-* Return: node
-*/
-
-t_cell *add_node(t_cell **tpt, double v)
-{
-t_cell *tempnode;
-
-tempnode = *tpt;
-
-/* firts time */
-	if (tempnode == NULL)
-	{
-	tempnode = malloc(sizeof(t_cell));
-	/* malloc goes wrong */
-		if (tempnode == NULL)
-			free(tempnode);
-
-	tempnode->elt = v;
-	tempnode->next = NULL;
-	}
-	else
-	{
-	tempnode = malloc(sizeof(t_cell));
-		if (tempnode == NULL)
-			free(tempnode);
-
-	tempnode->elt = v;
-	tempnode->next = *tpt;
-	}
-/* pointer set to new node adress */
-*tpt = tempnode;
-
-return (tempnode);
-}
-
-/**
-* heron - Return Heron secuence
-* @p: double
-* @x0: double
-* Return: list
-*/
-/* receive params */
+ * heron - funtion that return the Heron sequence until having convergence
+ * with an error less or equal to 10^(-7)
+ *
+ * @p: Parameter with the number to compare the root
+ * @x0: Parameter with the start point
+ *
+ * Return: The head of the created linked
+ */
 t_cell *heron(double p, double x0)
 {
-t_cell *thenode = NULL, *list;
-double value = x0;
-double comp = p / 2;
-double aprox = 0;
+	double x1, error = 0;
+	t_cell *new, *head = NULL, *tmp = NULL;
 
-	while (comp != aprox)
+	x1 = x0;
+
+	new = malloc(sizeof(t_cell));
+	if (new == NULL)
+		return (NULL);
+	head = new;
+	new->elt = x1;
+	new->next = NULL;
+
+	error = ((x1 * x1) > p) ? (x1 * x1) - p : p - (x1 * x1);
+
+	if (error <= 0.0000001)
 	{
-	/* another way of do the heron */
-		aprox = comp;
-		comp = (p / aprox + aprox) / 2;
+		return (head);
 	}
 
-/* iterate over value */
-	while (comp != value)
-	{
-	/* do the math */
-	value = 0.5 * (value + (p / value));
+	x1 = ((p / x0) + x0) / 2;
 
-	/* Populate node list */
-	list = add_node(&thenode, value);
+	head = heron(p, x1);
+
+	tmp = head;
+
+	while (tmp->next)
+	{
+		tmp = tmp->next;
 	}
-/* Return list */
-return (list);
+
+	tmp->next = new;
+
+	return (head);
 }
