7ff67fd7c04cefcdc067af8d2ce0679b10eb64ae
Luis <luisgonzacc@gmail.com>
2022-01-07 03:36:27 +0000

    Task 7

--- 0x00-math_complex/7-division.c
+++ 0x00-math_complex/7-division.c
@@ -1,21 +1,18 @@
 #include "holberton.h"
-#include <math.h>
-
 /**
- * division - perform the division operation to complex numbers
- * @c1: first complex number
- * @c2: second complex number
- * @c3: quotient of complex numbers
- * Return: void
+ * division - function that performs the division operation to complex numbers
+ *
+ * @c1: Parameter structure 1
+ * @c2: Parameter structure 2
+ * @c3: Parameter pointer to structure
+ *
+ * Return: return the division
  */
-
 void division(complex c1, complex c2, complex *c3)
 {
-	double a = c1.re;
-	double b = c1.im;
-	double p = c2.re;
-	double q = c2.im;
+	c3->re = ((c1.re * c2.re) + (c1.im * c2.im));
+	c3->re /= ((c2.re * c2.re) + (c2.im * c2.im));
 
-	c3->re = ((a * p) + (b * q)) / (pow(p, 2) + pow(q, 2));
-	c3->im = ((b * p) - (a * q)) / (pow(p, 2) + pow(q, 2));
+	c3->im = ((c1.re * -c2.im) + (c1.im * c2.re));
+	c3->im /= ((c2.re * c2.re) + (c2.im * c2.im));
 }
